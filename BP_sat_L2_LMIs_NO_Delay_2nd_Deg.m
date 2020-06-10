cf977b64109a3dfeacc383daba2b076112737bae
S-Salavati <66709403+S-Salavati@users.noreply.github.com>
2020-06-10 11:43:10 -0500

    Update BP_sat_L2_LMIs_NO_Delay_2nd_Deg.m

--- BP_sat_L2_LMIs_NO_Delay_2nd_Deg.m
+++ BP_sat_L2_LMIs_NO_Delay_2nd_Deg.m
@@ -1,29 +1,16 @@
 clc
 clear
 
-% %
-% % figure;
-% % set(gca,'Linewidth',2);
-% % plot(u(:,1),u(:,2),'Linewidth',2);
-% % set(0,'DefaultTextInterpreter','Latex');set(gca,'FontSize',26);232
-% % set(gca,'TicklabelInterpreter','Latex')
-% % set( get( gca, 'XLabel' ), 'Interpreter', 'Latex' );
-% % set( get( gca, 'YLabel' ), 'Interpreter', 'Latex' );
-% % title('Control effort','FontSize', 26);
-% % xlabel('Time [sec]','FontSize', 26);
-% % ylabel('u(t)','FontSize', 26);
+addpath(genpath('/home/saeed/Downloads/YALMIP')); % Path to YALMIP
+addpath(genpath('/home/saeed/Downloads/mosek/9.0/toolbox/r2015a/'));  % Path MOSEK
 
-addpath(genpath('/home/saeed/Downloads/YALMIP'));
-addpath(genpath('/home/saeed/Downloads/mosek/9.0/toolbox/r2015a/'));
-%run /home/saeed/Downloads/cvx/cvx_startup.m
 yalmip('clear')
-% load('EngSpeed.mat')
-% load('GridData.mat')
 
+% Constants
 inequality=1e-8;
-kappa=5;%4.9;%
 delta_1=1e-12; % Diagonal small term
 
+% Weights
 W_e=.9;%.2; Tracking error weight
 W_u=.01;%2;% Input weight
 
@@ -34,13 +21,14 @@ n_u=size(B_p,2);
 n_z=size(C_zp,1);
 n_y=size(C_yp,1);
 
-%%% Saturation
-u_bar=120;
+%%% Saturation data
+u_bar=120; % Saturation bound
 delta=4e-5;% snorm(w(t),2)^-2
-control=1;
 
-%%%% Uncertainty
-epsilon=300;%289;%
+
+%%%% Tuning Parameters and Uncertainty
+kappa=5;%
+epsilon=300;%
 E_A=[.02,0;
     0,0];
 G_A=[.005,0;
@@ -59,9 +47,6 @@ theta_max=8;
 v_3=.5;
 theta_prime=1;
 
-% step_rho_1=(tau_max-tau_min)/4;
-% step_rho_2=(K_max-K_min)/4;
-% step_rho_3=(theta_max-theta_min)/4;
 
 for jj=1:size(epsilon,2)
     for j=1:size(kappa,2)
@@ -183,19 +168,14 @@ for jj=1:size(epsilon,2)
         LMI= blkdiag(W_bar,gamma_sq)>=inequality*eye(size(blkdiag(W_bar,gamma_sq)));
         LMI= LMI +(blkdiag([W_bar, S1_bar; S1_bar', W_bar],delta-beta)>=zeros(size(blkdiag([W_bar, S1_bar; S1_bar', W_bar],delta-beta))));%
         
-        for rho_1 = 100:50:200%tau_min:step_rho_1:tau_max
-            for rho_2 = [.45 .55 .66]%K_min:step_rho_2:K_max
-                %                 for rho_3 = rho_3_min:step_rho_3:rho_3_max
+        for rho_1 = 100:50:200
+            for rho_2 = [.45 .55 .66]
                 [A_p,B_p,D_p,C_yp,C_zp,D_zu,D_zw]=systemdynamics(rho_1,rho_2,delta_1,W_e,W_u);
                 C_yp_delayed=C_yp;
                 
                 X= X_0 + rho_1 * X_1 + 1/2 * rho_1^2 * X_2 + rho_2 * X_3 + 1/2 * rho_2^2 * X_4;
                 Y= Y_0 + rho_1 * Y_1 + 1/2 * rho_1^2 * Y_2 + rho_2 * Y_3 + 1/2 * rho_2^2 * Y_4;
-                %                 X_1=0;
-                %                 X_2=0;
-                %                 Y_1=0;
-                %                 Y_2=0;
-                
+                              
                 A_tilde = A_tilde_0 + rho_1 * A_tilde_1 + 1/2* rho_1^2 * A_tilde_2 + rho_2 * A_tilde_3 + 1/2* rho_2^2 * A_tilde_4;
                 B_tilde = B_tilde_0 + rho_1 * B_tilde_1 + 1/2* rho_1^2 * B_tilde_2 + rho_2 * B_tilde_3 + 1/2* rho_2^2 * B_tilde_4;
                 C_tilde = C_tilde_0 + rho_1 * C_tilde_1 + 1/2* rho_1^2 * C_tilde_2 + rho_2 * C_tilde_3 + 1/2* rho_2^2 * C_tilde_4;
@@ -213,12 +193,9 @@ for jj=1:size(epsilon,2)
                 
                 G_tilde = G_tilde_0 + rho_1 * G_tilde_1 + 1/2 * rho_1^2 * G_tilde_2 + rho_2 * G_tilde_3 + 1/2 * rho_2^2 * G_tilde_4;
                 
-                % beta =        beta_0 + rho * beta_1 + 1/2 * rho^2 * beta_2;
-                
-                for rho_1_d=100:50:200%tau_min:step_rho_1:tau_max
-                    for rho_2_d = [.45 .55 .66]%K_min:step_rho_2:K_max
-                        %                             for rho_3_d = rho_3_min:step_rho_3:rho_3_max
-                        
+                             
+                for rho_1_d=100:50:200
+                    for rho_2_d = [.45 .55 .66]
                         A_tilde_theta = A_tilde_theta_0 + rho_1 * A_tilde_theta_1 + rho_2 * A_tilde_theta_2 +  rho_1 * rho_2 * A_tilde_theta_3 ...
                             + rho_1_d * A_tilde_theta_4 + rho_2_d * A_tilde_theta_5 + rho_1_d * rho_2_d * A_tilde_theta_6;
                         
@@ -237,15 +214,7 @@ for jj=1:size(epsilon,2)
                         G_1_tilde_delayed = G_1_tilde_0 + rho_1_d * G_1_tilde_1 + 1/2 * rho_1_d^2 * G_1_tilde_2 + rho_2_d * G_1_tilde_3 + 1/2 * rho_2_d^2 * G_1_tilde_4;
                         
                         P_bar_delayed = P_bar_0 + rho_1_d * P_bar_1 + 1/2 * rho_1_d^2 * P_bar_2 + rho_2_d * P_bar_3 + 1/2 * rho_2_d^2 * P_bar_4;
-                        %         for counter=1:n_u
-                        %             LMI=LMI+([beta, [C_tilde_delayed(counter,:), D_K_delayed(counter,:) * C_yp]-G_hat_delayed(counter,:);...
-                        %                                 ([C_tilde(counter,:),D_K(counter,:) * C_yp]-G_hat(counter,:))', u_bar(counter)^2*P_bar_delayed]...
-                        %                 >=zeros(size([beta, [C_tilde_delayed(counter,:), D_K_delayed(counter,:) * C_yp]-G_hat_delayed(counter,:);...
-                        %                                 ([C_tilde(counter,:),D_K(counter,:) * C_yp]-G_hat(counter,:))', u_bar(counter)^2*P_bar_delayed])));
-                        %         end
-                        
-                        %             for rho_h=800:step_h:3500
-                        %                 S_bar_h = S_bar_0 + rho_h * S_bar_1 + 1/2 * rho_h^2 * S_bar_2;%%
+                      
                         S_bar_h=S_bar;
                         
                         A_hat_cl = [A_p*Y,A_p;A_tilde,X*A_p+B_tilde*C_yp];
@@ -326,14 +295,7 @@ for jj=1:size(epsilon,2)
                                     
                                     L_1010= -epsilon(jj)*eye(2*n);
                                     
-                                    %                 L =[L_11,  L_12,  L_13,  L_14,  L_15,  L_16,   L_17;
-                                    %                     L_12', L_22,  L_23,  L_24,  L_25,  L_26,   L_27;
-                                    %                     L_13', L_23', L_33,  L_34,  L_35,  L_36,   L_37;
-                                    %                     L_14', L_24', L_34', L_44,  L_45,  L_46,   L_47;
-                                    %                     L_15', L_25', L_35', L_45', L_55,  L_56,   L_57;
-                                    %                     L_16', L_26', L_36', L_46', L_56', L_66,   L_67;
-                                    %                     L_17', L_27', L_37', L_47', L_57', L_67',  L_77];
-                                    
+                                                                     
                                     L =[L_11,   L_12,   L_13,   L_14,   L_15,   L_16,   L_17,   L_18,   L_19,   L_110;
                                         L_12',  L_22,   L_23,   L_24,   L_25,   L_26,   L_27,   L_28,   L_29,   L_210;
                                         L_13',  L_23',  L_33,   L_34,   L_35,   L_36,   L_37,   L_38,   L_39,   L_310;
@@ -344,23 +306,16 @@ for jj=1:size(epsilon,2)
                                         L_18',  L_28',  L_38',  L_48',  L_58',  L_68',  L_78',  L_88,   L_89,   L_810;
                                         L_19',  L_29',  L_39',  L_49',  L_59',  L_69',  L_79',  L_89',  L_99,   L_910;
                                         L_110', L_210', L_310', L_410', L_510', L_610', L_710', L_810', L_910', L_1010];
-                                    
-                                    
-                                    
-                                    %                 LMI=LMI+(blkdiag(P_bar,Q_bar,Q_bar_delayed,S_bar,S_bar_h,T_bar_delayed)>=...
-                                    %                     1e-8*eye(size(blkdiag(P_bar,Q_bar,Q_bar_delayed,S_bar,S_bar_h,T_bar_delayed))));
-                                    LMI=LMI+(L<=-inequality * eye(size(L)));
+                                   
+                                     LMI=LMI+(L<=-inequality * eye(size(L)));
                                 end
-                                %                  LMI=LMI+(S_bar_h>=1e-8*eye(size(S_bar_h)));
                             end
-                            %                                     LMI=LMI+(blkdiag(Q_bar_delayed,T_bar_delayed)>=1e-8*eye(size(blkdiag(Q_bar_delayed,T_bar_delayed))));
                         end
                         for counter=1:n_u
                             LMI_sat_d=[beta, [C_tilde_delayed(counter,:), D_K_delayed(counter,:) * C_yp]-G_1_tilde_delayed(counter,:);...
                                 ([C_tilde_delayed(counter,:),D_K_delayed(counter,:) * C_yp]-G_1_tilde_delayed(counter,:))', u_bar(counter)^2*P_bar_delayed];
                             LMI=LMI + (LMI_sat_d>=0);
                         end
-                        %                             end
                     end
                 end
                 for counter=1:n_u
@@ -369,12 +324,12 @@ for jj=1:size(epsilon,2)
                     LMI=LMI+(LMI_sat>=0);
                 end
                 LMI=LMI+(blkdiag(Q_bar,S_bar)>=inequality*eye(size(blkdiag(Q_bar,S_bar))));
-                %                 end
             end
         end
         
         
         ops=sdpsettings('solver','mosek','debug',1);
+        %%% These are mosek variables
 %         ops.mosek.MSK_DPAR_PRESOLVE_TOL_X=1e-5;
 %         ops.mosek.MSK_DPAR_INTPNT_CO_TOL_DFEAS=1e-5;
 %         ops.mosek.MSK_DPAR_INTPNT_CO_TOL_MU_RED=1e-5;
@@ -382,9 +337,10 @@ for jj=1:size(epsilon,2)
         ops.mosek.MSK_DPAR_INTPNT_CO_TOL_PFEAS=1e-5;
 %         ops.mosek.MSK_DPAR_INTPNT_CO_TOL_REL_GAP=1e-6;  
         sol=optimize(LMI,gamma_sq,ops);
-%         kappa_counter=j;
+
+        kappa_counter=j;
         kapp=kappa(j)
-%         epsilon_counter=jj;
+        epsilon_counter=jj;
         epsil=epsilon(jj)
         sol
         
@@ -443,7 +399,7 @@ for jj=1:size(epsilon,2)
             
             T_1_bar_0= value(T_1_bar_0);T_1_bar_1= value(T_1_bar_1);T_1_bar_2= value(T_1_bar_2);T_1_bar_3= value(T_1_bar_3);T_1_bar_4= value(T_1_bar_4);
         end
-        beta=value(beta) % beta_0=value(beta_0);beta_1=value(beta_1);beta_2=value(beta_2);
+        beta=value(beta)
         yalmip('clear')
     end
     
@@ -455,9 +411,6 @@ A_p=[-1/rho_1,0;
 B_p=[rho_2/rho_1;0];
 D_p=[0,rho_2/rho_1;
     1,0];% Omega=[r;d]
-%   D_p=[0,1;
-%         1,0;
-%         0,0];
 C_yp=[0,1];
 C_zp=[0,W_e;
     0,0]; % z=[e;u] = [r-C_yp*x;u]=[-C_yp;0]x + [0;1] u + [1 0;0 0][r;d]
@@ -465,29 +418,3 @@ D_zu=[0;W_u];
 D_zw=[0,0;
     0,0];
 end
-
-
-%
-%  figure;subplot(2,1,1);plot(track(:,1),track(:,[3,2]));
-%     set(0,'DefaultTextInterpreter','Latex');set(gca,'FontSize',20);
-%     set(gca,'TicklabelInterpreter','Latex');
-%     set( get( gca, 'XLabel' ), 'Interpreter', 'Latex' );
-%     set( get( gca, 'YLabel' ), 'Interpreter', 'Latex' );
-%     title('Tracking performance','FontSize', 24);
-%     ylabel('AFR','FontSize', 22);
-%    % xlabel('Time [s]','FontSize', 22);
-%     L=legend('Output AFR.','Ref. AFR');
-%     set(L,'Interpreter','Latex');
-%     grid;
-%
-%     subplot(2,1,2);plot(u(:,1),u(:,2));
-%     set(0,'DefaultTextInterpreter','Latex');set(gca,'FontSize',20);
-%     set(gca,'TicklabelInterpreter','Latex');
-%     set( get( gca, 'XLabel' ), 'Interpreter', 'Latex' );
-%     set( get( gca, 'YLabel' ), 'Interpreter', 'Latex' );
-%     title('Control effort','FontSize', 24);
-%     ylabel('$u(t)$','FontSize', 22);
-%     xlabel('Time [s]','FontSize', 22);
-%     %L=legend('$J_{th_2}$','$\Vert r_2(t) \Vert_{2,\tau}$');
-%     set(L,'Interpreter','Latex');
-%     grid;
